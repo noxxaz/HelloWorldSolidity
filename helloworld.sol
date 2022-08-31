6fbccb5cd55cc379f6131b0cc969ba4ceb288fb3
Zach Brown <zach@zach.tokyo>
2022-08-31 12:23:27 +0900

    Change message from public data to a pure function

--- helloworld.sol
+++ helloworld.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.8; // Require Solidity v0.8.8 or above
 
 contract HelloWorld {
 
-    string public message = "Hello World!";
-
     string public customMessage = "Custom message not set";
 
     function setMessage(string memory _message) public  { 
         customMessage = _message;
+    }
+
+    function message() public pure returns (string memory) {
+    
+        string memory hwMessage = "Hello World!";
+        return hwMessage;
+
     }
 }
